4abb39b3e5417ac925d64e1068bddc6f16eb8940
CarelessOutcast <tapiacarlos32@gmail.com>
2023-12-28 00:45:54 -0600

    Init commit

---
+++ WWW-HabitifyIntegration.js
@@ -0,0 +1,12 @@
+
+Module.register("WWW-HabitifyIntegration", {
+  defaults: {
+    text: "Hello World",
+  }
+  getDom: function () {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = this.config.text; 
+    return wrapper;
+  }
+});
+
