eb901d248ac630926d796e50e652ed7ec83e49e2
CarelessOutcast <tapiacarlos32@gmail.com>
2023-12-28 00:51:33 -0600

    quick fix

--- WWW-HabitifyIntegration.js
+++ WWW-HabitifyIntegration.js
@@ -2,11 +2,11 @@
 Module.register("WWW-HabitifyIntegration", {
   defaults: {
     text: "Hello World",
-  }
+  }, 
   getDom: function () {
     var wrapper = document.createElement("div");
     wrapper.innerHTML = this.config.text; 
     return wrapper;
-  }
+  },
 });
 
