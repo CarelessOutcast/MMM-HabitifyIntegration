1fbc3eaf8fbf5ca0987ab51589c6f60c8d3b7d33
CarelessOutcast <tapiacarlos32@gmail.com>
2024-01-01 22:48:51 -0600

    update

--- node_helper.js
+++ node_helper.js
@@ -3,6 +3,6 @@ const NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
   start: function() {
 	this.mySpecialProperty = "So much wow!";
-	Log.log(this.name + " is started!");
-},
+	console.log("TEST MODULE: " + this.name);
+  },
 });
